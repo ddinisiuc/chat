6d106f9c2fe7a9f966b13f34380e196756d6c7f2
ddinisiuc <flyer_treiser@mail.ru>
2020-10-13 11:19:49 +0300

    welcome added title

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,3 +1,3 @@
 @foreach ($data as $item)
-    <img width="400px" height="250px" src="{{ $item['source'] }}" alt="item titl">
+    <img width="400px" height="250px" src="{{ $item['source'] }}" alt="item ti">
 @endforeach
