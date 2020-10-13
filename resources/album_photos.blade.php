65f13764f3793fcc3fac5e434a5a0519848518e4
ddinisiuc <flyer_treiser@mail.ru>
2020-10-13 18:07:38 +0300

    fixed conflict

--- resources/album_photos.blade.php
+++ resources/album_photos.blade.php
@@ -1,8 +1,10 @@
 @extends('layouts.app')
 @section('content')
     <h2>Album</h2>
-    <div>master brancj</div>
+    <div>dev branch</div>
     @foreach ($data as $item)
         <img src="{{ $item->source }}" alt="">
     @endforeach
 @endsection
+soome changes uncommited (stash) +
+stash))
