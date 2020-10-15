a1405279b3bf61454ff43b680340520a1cf07d4a
ddinisiuc <flyer_treiser@mail.ru>
2020-10-15 16:14:21 +0300

    fix

--- resources/album_photos.blade.php
+++ resources/album_photos.blade.php
@@ -1,6 +1,7 @@
 @extends('layouts.app')
 @section('content')
     <h2>Album</h2>
+    <div>master brancj</div>
     <div>dev branch</div>
     @foreach ($data as $item)
         <img src="{{ $item->source }}" alt="">
